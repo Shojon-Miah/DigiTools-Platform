95d36f4baab2ed5fcf84a8cc0b5432bb82c61b52
Shojon-Miah <sujonv21@gmail.com>
2026-03-31 19:21:15 +0600

    add product data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
-import Steps from './components/Steps'
 import Stats from './components/Stats'
+import ProductSection from './components/ProductSection'
+import Steps from './components/Steps'
 
 function App() {
-  const [cartItems] = useState([])
+  const [cartItems, setCartItems] = useState([])
+
+  const handleAddToCart = (product) => {
+    const alreadyInCart = cartItems.some((item) => item.id === product.id)
+    if (!alreadyInCart) {
+      setCartItems([...cartItems, product])
+    }
+  }
 
   return (
     <div>
@@ -13,6 +21,7 @@ function App() {
       <Banner />
       <Steps />
       <Stats />
+      <ProductSection cartItems={cartItems} onAddToCart={handleAddToCart} />
     </div>
   )
 }
